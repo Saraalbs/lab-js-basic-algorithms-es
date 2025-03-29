cf1af6dbb79aeac21bc0aa110b367ba9733268e0
Sara <sara.albores.perez@gmail.com>
2025-03-29 12:00:28 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+let hacker1 = "Javi";
+let conductor = `El nombre del controlador es ${hacker1}`;
+console.log(conductor);
 
+let hacker2 = "Berto";
+let navegante = `El nombre del navegante es ${hacker2}`;
+console.log(navegante);
 
-// Iteration 3: Loops
+// console.log(hacker1.length)
+// console.log(hacker2.length)
+
+if (hacker1.length >= hacker2.length) {
+  console.log(
+    `El conductor tiene el nombre más largo, ${hacker1.length} tiene  caracteres`
+  );
+} else if (hacker1.length <= hacker2.length) {
+  console.log(
+    `Parece que el navegante tiene el nombre más largo, tiene ${hacker2.length} caracteres`
+  );
+} else {
+  console.log(
+    `¡Vaya, ambos tienen nombres igual de largos, ${hacker1.length} caracteres!`
+  );
+}
+
+console.log(hacker1.toUpperCase().split(""));
+console.log(hacker2.split("").reverse());
+
+let v1 = "El nombre del conductor va primero.";
+let v2 = "Yo, el navegador va primero definitivamente.";
+let v3 = "¿Qué? ¿Los dos tienen el mismo nombre?";
+
+let cadenas = [v1, v2, v3];
+cadenas.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+console.log(cadenas);
